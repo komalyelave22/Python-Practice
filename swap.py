efbc3ac6f8f5712d3076f8a4091986de8471b46b
komalyelave22 <komalyelave786@gmail.com>
2025-09-12 21:00:34 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+
+a=a+b
+b=a-b
+a=a-b
+
+
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
